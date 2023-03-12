0fea319ea7a5de4651b4d4e84cd7bd116294f08b
Yi Zong <zongyi@qixin.ai>
2023-03-12 17:59:26 +0800

    cors

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module accounts
 go 1.20
 
 require (
+	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-contrib/sessions v0.0.5
 	github.com/gin-gonic/gin v1.9.0
 	github.com/go-jose/go-jose/v3 v3.0.0
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"accounts/data"
 	"accounts/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
@@ -13,6 +14,7 @@ var secret = []byte("secret")
 
 func main() {
 	r := gin.Default()
+	r.Use(cors.Default())
 	r.Use(sessions.Sessions("QixinAuth", cookie.NewStore(secret)))
 	routes.AddRoutes(&r.RouterGroup)
 	if err := data.InitDB(); err != nil {
