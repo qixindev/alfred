8753fd1de2cc94c0e27c782b6015012a7ec9e1d6
jzf <jzf@qixin.ai>
2023-05-19 15:22:52 +0800

    wecom domain check

--- middlewares/wecom_domain.go
+++ middlewares/wecom_domain.go
@@ -8,7 +8,7 @@ import (
 func WecomDomainCheck() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		path := c.Request.URL.Path
-		if path == "WW_verify_lPcI9c2g5B1tINHE.txt" {
+		if path == "/WW_verify_lPcI9c2g5B1tINHE.txt" {
 			c.String(http.StatusOK, "lPcI9c2g5B1tINHE")
 			c.Abort()
 			return
