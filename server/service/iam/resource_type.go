4bda8788352d838861d26f80ea19c880c69e5ffa
jzf <jzf@qixin.ai>
2023-06-26 15:48:34 +0800

    delete resource type

--- server/service/iam/resource_type.go
+++ server/service/iam/resource_type.go
@@ -25,6 +25,34 @@ func CreateResourceType(tenantId uint, clientId string, resourceType *models.Res
 }
 
 func DeleteResourceType(tenantId uint, typeId string) error {
+	var roles []uint
+	if err := global.DB.Select("id").
+		Where("tenant_id = ? and type_id = ?", tenantId, typeId).
+		Find(&roles).Error; err != nil {
+		return err
+	}
+
+	if err := global.DB.Where("tenant_id = ? AND role_id IN ?", tenantId, roles).
+		Delete(&models.ResourceTypeRoleAction{}).Error; err != nil {
+		return err
+	}
+	if err := global.DB.Where("tenant_id = ? AND role_id IN ?", tenantId, roles).
+		Delete(&models.ResourceRoleUser{}).Error; err != nil {
+		return err
+	}
+	if err := global.DB.Where("tenant_id = ? AND id IN ?", tenantId, roles).
+		Delete(&models.ResourceTypeRole{}).Error; err != nil {
+		return err
+	}
+
+	if err := global.DB.Where("tenant_id = ? AND type_id = ?", tenantId, typeId).
+		Delete(&models.ResourceTypeAction{}).Error; err != nil {
+		return err
+	}
+	if err := global.DB.Where("tenant_id = ? AND type_id = ?", tenantId, typeId).
+		Delete(&models.Resource{}).Error; err != nil {
+		return err
+	}
 	if err := global.DB.Where("tenant_id = ? AND id = ?", tenantId, typeId).
 		Delete(&models.ResourceType{}).Error; err != nil {
 		return err
