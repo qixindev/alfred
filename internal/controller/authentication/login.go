af72e871798950cf350e02d65251358e3f76c48d
jzf <jzf@qixin.ai>
2023-08-31 15:39:56 +0800

    add login proto

--- internal/controller/authentication/login.go
+++ internal/controller/authentication/login.go
@@ -129,8 +129,30 @@ func Logout(c *gin.Context) {
 	resp.Success(c)
 }
 
+// GetLoginProtocol godoc
+//
+//	@Summary	logout current user
+//	@Schemes
+//	@Description	logout current user
+//	@Tags			login
+//	@Param			tenant	path	string	true	"tenant"	default(default)
+//	@Success		200
+//	@Router			/accounts/{tenant}/login/proto [get]
+func GetLoginProtocol(c *gin.Context) {
+	resp.SuccessWithArrayData(c, []gin.H{
+		{
+			"name": "用户服务协议",
+			"url":  "https://devbackup.blob.core.chinacloudapi.cn/picture/%E7%94%A8%E6%88%B7%E6%9C%8D%E5%8A%A1%E5%8D%8F%E8%AE%AE.md?sp=r&st=2023-08-31T07:30:43Z&se=2123-08-31T15:30:43Z&spr=https&sv=2022-11-02&sr=b&sig=PsaLy37JxY6Buhp9z9fwv6PiofeKbCwGzZNv7iI2bg4%3D",
+		}, {
+			"name": "隐私保护声明",
+			"url":  "https://devbackup.blob.core.chinacloudapi.cn/picture/%E9%9A%90%E7%A7%81%E4%BF%9D%E6%8A%A4%E5%A3%B0%E6%98%8E.md?sp=r&st=2023-08-31T07:24:53Z&se=2123-08-31T15:24:53Z&spr=https&sv=2022-11-02&sr=b&sig=Jpd16wFlwtpF7GNry%2FsH8vDQVpoDWOERRjm273QGR7I%3D",
+		},
+	}, 2)
+}
+
 func AddLoginRoutes(rg *gin.RouterGroup) {
 	rg.POST("/login", Login)                    // 账号密码登录
+	rg.GET("/login/proto", GetLoginProtocol)    // 获取登录隐私协议
 	rg.GET("/login/:provider", LoginToProvider) // 第三方登录重定向
 	rg.GET("/providers", ListProviders)         // 第三方信息
 	rg.GET("/providers/:provider", GetProvider) // 第三方具体信息
