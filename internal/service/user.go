7910bd4f2f0e987a4141564b0dd87f987d31075a
jzf <jzf@qixin.ai>
2023-12-04 14:44:51 +0800

    provider avatar

--- internal/service/user.go
+++ internal/service/user.go
@@ -91,6 +91,7 @@ func BindLoginUser(userInfo *model.UserInfo, tenantId uint, userFrom string) (us
 		Disabled:         false,
 		TenantId:         tenantId,
 		From:             userFrom,
+		Avatar:           userInfo.Picture,
 		Meta:             "{}",
 	}
 	if newUser.Username == "" {
