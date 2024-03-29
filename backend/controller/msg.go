6005b2e74ee029f5387032960bafbbef0f29607e
Jiarui Rao <rjr@qixin.ai>
2024-03-29 10:57:10 +0800

    0329 回退发送消息接口

--- backend/controller/msg.go
+++ backend/controller/msg.go
@@ -5,10 +5,15 @@ import (
 	"alfred/backend/endpoint/resp"
 	"alfred/backend/model"
 	"alfred/backend/pkg/client/msg/notify"
+	"alfred/backend/pkg/config/env"
+	"alfred/backend/pkg/global"
 	"alfred/backend/service"
-	"github.com/gin-gonic/gin"
+	"alfred/backend/service/auth"
 	"strconv"
 	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 // SendMsg
@@ -26,14 +31,71 @@ func SendMsg(c *gin.Context) {
 		return
 	}
 
-	msgService := service.NewMsgService()
-	providerId := c.Param("providerId")
-	msgService.Tenant = internal.GetTenant(c)
-	in.TenantId = msgService.Tenant.Id
+	var providerUser []string
+	var providerConfig gin.H
 
-	providerConfig, err := msgService.ProcessMsg(providerId, in)
-	if err != nil {
-		resp.ErrorUnknown(c, err, "failed to process msg")
+	tenant := internal.GetTenant(c)
+	in.TenantId = tenant.Id
+	providerId := c.Param("providerId")
+
+	if providerId == "0" { // 站内消息
+		providerConfig = make(gin.H)
+		providerConfig["type"] = env.PlatformAlfred
+	} else {
+		var p model.Provider
+		if err := internal.TenantDB(c).First(&p, "id = ?", providerId).Error; err != nil {
+			resp.ErrorSqlFirst(c, err, "get provider err")
+			return
+		}
+
+		_, authProvider, err := auth.GetAuthProvider(tenant.Id, p.Name)
+		if err != nil {
+			resp.ErrorUnknown(c, err, "get provider err")
+			return
+		}
+
+		providerConfig = *authProvider.ProviderConfig()
+		usersSlice := make([]string, len(in.Users))
+		for i, v := range in.Users {
+			usersSlice[i] = strings.Trim(v, "{}")
+		}
+		if err = global.DB.Table("provider_users pu").Select("pu.name").
+			Joins("LEFT JOIN client_users as cu ON cu.user_id = pu.user_id").
+			Where("pu.tenant_id = ? AND pu.provider_id = ? AND cu.sub IN (?)", tenant.Id, providerConfig["providerId"], usersSlice).
+			Find(&providerUser).Error; err != nil {
+			resp.ErrorSqlSelect(c, err, "get provider user err")
+			return
+		}
+	}
+
+	var userDb []model.SendInfo
+	for _, v := range in.Users {
+		userDb = append(userDb, model.SendInfo{
+			Link:       in.Link,
+			UsersDB:    v,
+			Sender:     in.Sender,
+			Platform:   providerConfig["type"].(string),
+			TenantId:   tenant.Id,
+			Msg:        in.Msg,
+			MsgType:    in.MsgType,
+			Title:      in.Title,
+			TitleColor: in.TitleColor,
+			PngLink:    in.PngLink,
+			SendAt:     time.Now(),
+		})
+	}
+
+	// 调用InsertSendInfo函数插入数据到数据库
+	if err := global.DB.Create(&userDb).Error; err != nil {
+		resp.ErrorSqlCreate(c, err, "failed to insert SendInfo")
+		return
+	}
+
+	in.Users = providerUser
+	in.Platform = providerConfig["type"].(string)
+	if len(in.Users) == 0 {
+		global.LOG.Warn("no provider user")
+		resp.SuccessWithMessage(c, "no provider user")
 		return
 	}
 
@@ -55,19 +117,52 @@ func SendMsg(c *gin.Context) {
 // @Success	200
 // @Router	/accounts/{tenant}/message/getMsg/{subId} [get]
 func GetMsg(c *gin.Context) {
-	msgService := service.NewMsgService()
-	msgService.Tenant = internal.GetTenant(c)
+	var SendInfo []model.SendInfo
+	var SendInfoDB []model.SendInfoDB
 
+	tenant := internal.GetTenant(c)
 	subId := c.Param("subId")
 	msgTypes := strings.Split(c.Query("msgTypes"), ",")
+
 	pageNum, _ := strconv.Atoi(c.DefaultQuery("pageNum", "1"))
 	pageSize, _ := strconv.Atoi(c.DefaultQuery("pageSize", "10"))
 
-	list, total, err := msgService.GetMsgList(subId, msgTypes, pageNum, pageSize)
-	if err != nil {
+	query := global.DB.Table("message").
+		Select("message.*, u1.display_name as sender_name, u2.display_name as receiver_name, u1.avatar").
+		Joins("LEFT JOIN client_users cu1 ON message.sender = cu1.sub").
+		Joins("LEFT JOIN client_users cu2 ON message.users_db = cu2.sub").
+		Joins("LEFT JOIN users u1 ON cu1.user_id = u1.id").
+		Joins("LEFT JOIN users u2 ON cu2.user_id = u2.id").
+		Where("message.users_db = ? AND message.tenant_id = ?", subId, tenant.Id)
+
+	if len(msgTypes) > 0 && msgTypes[0] != "" {
+		query = query.Where("message.msg_type IN ?", msgTypes)
+	}
+
+	if err := query.Limit(pageSize).Offset((pageNum - 1) * pageSize).Order("send_at desc").
+		Find(&SendInfoDB).Error; err != nil {
+		resp.ErrorSqlSelect(c, err, "failed to get msg")
 		return
 	}
-	resp.SuccessWithDataAndTotal(c, list, total)
+
+	var total int64
+	if err := internal.TenantDB(c).Model(&model.SendInfo{}).
+		Where("users_db = ? AND msg_type IN (?)", subId, msgTypes).
+		Find(&SendInfo).Count(&total).Error; err != nil {
+		resp.ErrorSqlSelect(c, err, "failed to get msg")
+		return
+	}
+
+	for i, v := range SendInfoDB {
+		for _, v2 := range SendInfo {
+			if v.Id == v2.Id {
+				SendInfo[i].SenderName = v2.SenderName
+				SendInfo[i].ReceiverName = v2.ReceiverName
+			}
+		}
+	}
+
+	resp.SuccessWithDataAndTotal(c, SendInfoDB, total)
 }
 
 // MarkMsg
@@ -78,16 +173,13 @@ func GetMsg(c *gin.Context) {
 // @Success	200
 // @Router	/accounts/{tenant}/message/markMsg/{msgId} [put]
 func MarkMsg(c *gin.Context) {
-	msgService := service.NewMsgService()
-	msgService.Tenant = internal.GetTenant(c)
-
 	var in model.SendInfo
 	if err := c.ShouldBindUri(&in); err != nil {
 		resp.ErrReqPara(c, err)
 		return
 	}
-
-	if err := msgService.MarkMsg(in.Id, true); err != nil {
+	tenant := internal.GetTenant(c)
+	if err := service.MarkMsgAsRead(in.Id, tenant); err != nil {
 		resp.ErrorUnknown(c, err, "failed to mark msg read")
 		return
 	}
@@ -102,11 +194,9 @@ func MarkMsg(c *gin.Context) {
 // @Success	200
 // @Router	/accounts/{tenant}/unreadMsgCount/{subId} [get]
 func GetUnreadMsgCount(c *gin.Context) {
-	msgService := service.NewMsgService()
-	msgService.Tenant = internal.GetTenant(c)
-
 	subId := c.Param("subId")
-	count, err := msgService.GetMsgCount(subId, false)
+	tenant := internal.GetTenant(c)
+	count, err := service.GetUnreadMsgCount(subId, tenant)
 	if err != nil {
 		resp.ErrorUnknown(c, err, "failed to get unread msg count")
 		return
--- backend/service/msg.go
+++ backend/service/msg.go
@@ -2,144 +2,27 @@ package service
 
 import (
 	"alfred/backend/model"
-	"alfred/backend/pkg/config/env"
 	"alfred/backend/pkg/global"
-	"alfred/backend/service/auth"
 	"errors"
-	"github.com/gin-gonic/gin"
-	"strings"
-	"time"
 )
 
-type MsgService struct {
-	Tenant *model.Tenant
-}
-
-func NewMsgService() *MsgService {
-	return &MsgService{}
-}
-
-func (ms *MsgService) ProcessMsg(providerId string, in model.SendInfo) (gin.H, error) {
-	var providerUser []string
-	var providerConfig gin.H
-
-	if providerId == "0" { // 站内消息
-		providerConfig = make(gin.H)
-		providerConfig["type"] = env.PlatformAlfred
-	} else {
-		var p model.Provider
-		if err := global.DB.First(&p, "id = ? AND tenant_id = ?", providerId, ms.Tenant.Id).Error; err != nil {
-			return gin.H{}, err
-		}
-
-		_, authProvider, err := auth.GetAuthProvider(ms.Tenant.Id, p.Name)
-		if err != nil {
-			return gin.H{}, err
-		}
-
-		providerConfig = *authProvider.ProviderConfig()
-		usersSlice := make([]string, len(in.Users))
-		for i, v := range in.Users {
-			usersSlice[i] = strings.Trim(v, "{}")
-		}
-		if err = global.DB.Table("provider_users pu").Select("pu.name").
-			Joins("LEFT JOIN client_users as cu ON cu.user_id = pu.user_id").
-			Where("pu.tenant_id = ? AND pu.provider_id = ? AND cu.sub IN (?)", ms.Tenant.Id, providerConfig["providerId"], usersSlice).
-			Find(&providerUser).Error; err != nil {
-			return gin.H{}, err
-		}
-	}
-
-	var userDb []model.SendInfo
-	for _, v := range in.Users {
-		userDb = append(userDb, model.SendInfo{
-			Link:       in.Link,
-			UsersDB:    v,
-			Sender:     in.Sender,
-			Platform:   providerConfig["type"].(string),
-			TenantId:   ms.Tenant.Id,
-			Msg:        in.Msg,
-			MsgType:    in.MsgType,
-			Title:      in.Title,
-			TitleColor: in.TitleColor,
-			PngLink:    in.PngLink,
-			SendAt:     time.Now(),
-		})
-	}
-
-	// 调用InsertSendInfo函数插入数据到数据库
-	if err := global.DB.Create(&userDb).Error; err != nil {
-		return gin.H{}, err
-	}
-
-	in.Users = providerUser
-	in.Platform = providerConfig["type"].(string)
-	if len(in.Users) == 0 {
-		global.LOG.Warn("no provider user")
-		return providerConfig, errors.New("no provider user")
-	}
-	return providerConfig, nil
-}
-
-func (ms *MsgService) GetMsgList(subId string, msgTypes []string, pageNum int, pageSize int) ([]model.SendInfoDB, int64, error) {
-	var sendInfoDB []model.SendInfoDB
-	var SendInfo []model.SendInfo
-
-	sendInfoDBQuery := global.DB.Table("message").
-		Select("message.*, u1.display_name as sender_name, u2.display_name as receiver_name, u1.avatar").
-		Joins("LEFT JOIN client_users cu1 ON message.sender = cu1.sub").
-		Joins("LEFT JOIN client_users cu2 ON message.users_db = cu2.sub").
-		Joins("LEFT JOIN users u1 ON cu1.user_id = u1.id").
-		Joins("LEFT JOIN users u2 ON cu2.user_id = u2.id").
-		Where("message.users_db = ? AND message.tenant_id = ?", subId, ms.Tenant.Id)
-
-	sendInfoQuery := global.DB.Model(&model.SendInfo{}).
-		Where("users_db = ? AND tenant_id = ?", subId, ms.Tenant.Id)
-
-	if len(msgTypes) > 0 && msgTypes[0] != "" {
-		sendInfoDBQuery = sendInfoDBQuery.Where("message.msg_type IN ?", msgTypes)
-		sendInfoQuery = sendInfoQuery.Where("message.msg_type IN ?", msgTypes)
-	}
-
-	if err := sendInfoDBQuery.Limit(pageSize).Offset((pageNum - 1) * pageSize).Order("send_at desc").
-		Find(&sendInfoDB).Error; err != nil {
-		return nil, 0, err
-	}
-
-	var total int64
-	if err := sendInfoQuery.Limit(pageSize).Offset((pageNum - 1) * pageSize).Order("send_at desc").
-		Find(&SendInfo).Count(&total).Error; err != nil {
-		return nil, 0, err
-	}
-
-	for i, v := range sendInfoDB {
-		for _, v2 := range SendInfo {
-			if v.Id == v2.Id {
-				SendInfo[i].SenderName = v2.SenderName
-				SendInfo[i].ReceiverName = v2.ReceiverName
-			}
-		}
-	}
-	return sendInfoDB, total, nil
-}
-
-func (ms *MsgService) MarkMsg(msgId uint, isRead bool) error {
+func MarkMsgAsRead(msgId uint, tenant *model.Tenant) error {
 	var count int64
-	if err := global.DB.Model(&model.SendInfo{}).Where("id = ? AND tenant_id = ?", msgId, ms.Tenant.Id).Count(&count).Error; err != nil {
+	if err := global.DB.Model(&model.SendInfo{}).Where("id = ? AND tenant_id = ?", msgId, tenant.Id).Count(&count).Error; err != nil {
 		return err
 	}
 	if count == 0 {
 		return errors.New("msg not found")
 	}
-	if err := global.DB.Model(&model.SendInfo{}).Where("id = ? AND tenant_id = ?", msgId, ms.Tenant.Id).Update("is_read", isRead).Error; err != nil {
+	if err := global.DB.Model(&model.SendInfo{}).Where("id = ? AND tenant_id = ?", msgId, tenant.Id).Update("is_read", true).Error; err != nil {
 		return err
 	}
 	return nil
 }
 
-func (ms *MsgService) GetMsgCount(subId string, isRead bool) (int64, error) {
+func GetUnreadMsgCount(subId string, tenant *model.Tenant) (int64, error) {
 	var count int64
-	if err := global.DB.Model(&model.SendInfo{}).Where("users_db = ? AND is_read = ? AND tenant_id = ?", subId, isRead, ms.Tenant.Id).Count(&count).Error; err != nil {
+	if err := global.DB.Model(&model.SendInfo{}).Where("users_db = ? AND is_read = ? AND tenant_id = ?", subId, false, tenant.Id).Count(&count).Error; err != nil {
 		return 0, err
 	}
 	return count, nil
